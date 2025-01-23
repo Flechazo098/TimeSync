f1eb527581b21191f6650112cbc7428fbf156447
Flechazo <2558755403@qq.com>
2025-01-23 13:04:34 +0800

    Updated to 1.21 and some optimizations have been made

--- src/main/java/be/cloudns/edebe/timesync/fabric/util/TimeUtil.java
+++ src/main/java/be/cloudns/edebe/timesync/fabric/util/TimeUtil.java
@@ -1,11 +1,20 @@
 package be.cloudns.edebe.timesync.fabric.util;
 
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 public final class TimeUtil {
+
     public static long getMinecraftTime() {
-        ZonedDateTime time = ZonedDateTime.now(ZoneId.of("GMT+2"));
-        return (long) ((time.getHour() * 60 * 60L + time.getMinute() * 60L + time.getSecond()) / 3.6);
+        // 使用系统默认时区获取当前时间
+        LocalTime now = LocalTime.now(ZoneId.systemDefault());
+        
+        // 优化计算方法：直接使用LocalTime的toSecondOfDay
+        long totalSeconds = now.toSecondOfDay();
+        
+        // 将秒数转换为Minecraft tick (1 second = 20 ticks)
+        // 3.6 = 72/20 (72 = 24 hours * 3600 seconds / 1200 minecraft hours)
+        return (long) (totalSeconds / 3.6);
     }
 }
