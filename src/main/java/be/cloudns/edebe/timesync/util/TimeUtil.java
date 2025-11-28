ca237df70a8756cf806550cf4055209345b7a897
Flechazo <2558755403@qq.com>
2025-11-29 05:07:19 +0800

    fix

--- src/main/java/be/cloudns/edebe/timesync/util/TimeUtil.java
+++ src/main/java/be/cloudns/edebe/timesync/util/TimeUtil.java
@@ -4,10 +4,16 @@ import java.time.LocalTime;
 import java.time.ZoneId;
 
 public final class TimeUtil {
+    private static final long TICKS_PER_DAY = 24000L;
+    private static final long SECONDS_PER_DAY = 24 * 60 * 60;
 
     public static long getMinecraftTime() {
         LocalTime now = LocalTime.now(ZoneId.systemDefault());
-        long totalSeconds = now.toSecondOfDay();
-        return (long) (totalSeconds / 3.6);
+        long secondsSinceMidnight = now.toSecondOfDay();
+
+        long adjustedSeconds = (secondsSinceMidnight - 6 * 3600 + SECONDS_PER_DAY) % SECONDS_PER_DAY;
+
+        double ticks = adjustedSeconds * (TICKS_PER_DAY * 1.0 / SECONDS_PER_DAY);
+        return (long) ticks;
     }
 }
